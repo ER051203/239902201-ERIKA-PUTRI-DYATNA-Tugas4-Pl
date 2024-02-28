9b0958aec1afd4fd66a3da96a7d023631ce17476
ERIKA PUTRI DYATNA <161329864+ER051203@users.noreply.github.com>
2024-02-28 11:04:40 +0700

    Add files via upload
    Tugas membuat program yang membaca suatu bilangan dan mencetak jumlah semua nilai dari 1 sampai dengan bilangan tersebut.

---
+++ task4.py
@@ -0,0 +1,7 @@
+print ("---MEMBACA DAN MENCETAK JUMLAH SEMUA NILAI---") 
+
+bilangan = int(input("Masukan Nilai : "))
+
+jumlah = (bilangan*(bilangan+1)) // 2       #menghitung jumlah semua nilai dari 1 sampai akhir bilangan tersebut
+
+print ("Jumlah semua nilai dari bilangan", bilangan, "adalah : ",jumlah )      #tampilan output program
